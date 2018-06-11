c44b1e18797bd52bd3b0f587ec70eaa143f147cb
Joseph Bagshaw <39792803+JosephBagshaw@users.noreply.github.com>
2018-06-11 15:48:30 +0100

    Generate random objects to add to a Security Group
    As part of our client firewall configuration change, we wanted to select 50 random computer objects from Active Directory to ensure deployment to a wide range of devices via GPO.
    
    Manually selecting and adding the objects is a repetitive and time consuming task so I turned to PowerShell. After setting out the tasks that I needed to achieve, building the script was a straightforward task:
    
    1.	Filter the objects by Operating System
    2.	Create separate hash tables for the computer objects and the GPO
    3.	Ensure already added objects are not selected for to the GPO
    4.	Randomly select objects from the hash table
    5.	Add selected objects to GPO.

--- Get-Random.ps1
+++
@@ -1,4 +0,0 @@
-$a = Get-ADComputer -Properties OperatingSystem -Filter { (OperatingSystem -Like "Windows 10 Enterprise") -or (OperatingSystem -Like "Windows 7 Enterprise") -and (Enabled -eq "True") }
-$f = Get-ADGroupMember "Security Group"
-$d = Compare-Object -ReferenceObject $a -DifferenceObject $f -PassThru | Get-Random -Count 50
-foreach ($computer in $d){Add-ADGroupMember -Identity "Security Group" -Members $computer}
